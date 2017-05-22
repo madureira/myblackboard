d2a31ca271c2b4d66deffa5b192faaa8e378c10e
Rafael Madureira <rafael.madureira.santos@gmail.com>
2017-05-21 21:04:44 -0300

    Removed unused libs

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,6 @@
 import React                    from 'react';
 import { render }               from 'react-dom';
 import { Router, hashHistory }  from 'react-router';
-import injectTapEventPlugin     from 'react-tap-event-plugin';
 import routes                   from './routes';
-
-injectTapEventPlugin();
 
 render((<Router routes={ routes } history={ hashHistory } />), document.getElementById('app'));
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import MuiThemeProvider     from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
+      <div>
         { this.props.children }
-      </MuiThemeProvider>
+      </div>
     );
   }
 
