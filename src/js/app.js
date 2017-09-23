b98df6532f79a5f36ea798b24f02132c0477a148
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2017-09-23 15:49:53 -0300

    Add Demo files

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-import React                    from 'react';
-import { render }               from 'react-dom';
-import { Router, hashHistory }  from 'react-router';
-import injectTapEventPlugin     from 'react-tap-event-plugin';
-import routes                   from './routes';
-
-injectTapEventPlugin();
-
-render((<Router routes={ routes } history={ hashHistory } />), document.getElementById('app'));
